8c36b33e4b7336580ac2118c1045f91ff2bfa9c7
emlo0002 <emilie.lofmark01@gmail.com>
2025-02-18 13:51:28 +0100

    Add files via upload

---
+++ tidslinje.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", function () {
+  runProgram();
+});
+
+async function runProgram() {
+  // 1. Load svg map
+  // json data fetch
+  let jsondata = await fetch("tid.json");
+  objekter = await jsondata.json();
+
+  // 1. Load svg map
+  let mySvg = await fetch("tid.svg");
+  let svg = await mySvg.text();
+
+  document.querySelector("#tid").innerHTML = svg;
+  //------------------------------------------------------------------------------------
+  // 2. find infobokse og skjul dem
+  //------------------------------------------------------------------------------
+  // 3. Skift farve ved klik, og vis tekst
+  //-----------------------------------------------------------------------
+  //function clicked
+  //--------------------------------------------------------------------
+  // a. find det klikkede element
+  //----------------------------------------------
+  // b. find det klikkede elementets ID
+  //---------------------------------------------
+  // c. find  det klikkede elements fillfarve
+  //---------------------------------------------
+  // d. vis infobokse
+  //--------------------------------------------
+  // 4. hvis der tidligere har været klikket skal det forige element skifte farve til original
+  //------------------------------------------------------------------------------------
+  //gør det klikkede til det aktive
+  //-------------------------------------------------------------------------
+  //skift farve på det valgte
+  //-------------------------------------------------------------------------
+  //reset farve og skjul tekst hvis valgt elementet allerede er aktivt
+  //--------------------------------------------------------------------------
+}
